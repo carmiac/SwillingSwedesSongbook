d4cc03958b105eeeb39812765a127ce5a6900dca
Adam Milner <carmiac@gmail.com>
2015-09-10 23:43:28 -0700

    Update makebook.py
    fixing the platform check
    adding some documentation

--- makebook.py
+++ makebook.py
@@ -7,23 +7,26 @@ import subprocess
 import platform
 import shutil
 
-# first, set up a bunch of paths for later
-if platform.system == 'Windows':
+## first, set up some paths for later
+# where to find the individual song files
+song_glob = os.sep.join(['src','*.txt']) 
+# header for the entire songbook
+sbd_header = "header.txt" 
+# name of the sorted song file
+sbd_out = "songs.sbd"
+# some os specific paths
+if platform.system() == 'Windows':
     songidx = '..\src\songidx.exe'
     bible = '..\src\songidx\bible.can'
     pdflatex = 'C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\pdflatex.exe'
-
-else:
+else: # must be *nix
     songidx = '../src/songidx/songidx'
     bible = '../src/songidx/bible.can'
     pdflatex = 'pdflatex'
+    # copy over the songs style file 
     shutil.copy2('../src/songs/songs.sty', './songs.sty')
 
-song_glob = os.sep.join(['src','*.txt'])
-sbd_header = "header.txt"
-sbd_out = "songs.sbd"
-
-# get the list of song files to add and sort it
+# get the list of song files and sort them
 filenames = sorted(glob.glob(song_glob))
 
 # create the sbd output file
@@ -36,16 +39,14 @@ with open(sbd_out, 'w') as fout:
         fout.write(line)
 
 # next, get a list of .tex files to use
-texfiles = sorted(glob.glob('*.tex'))
+texfiles = glob.glob('*.tex')
 
-# then, regenerate all .tex files, all index files, then all .tex files again
+# process all .tex files, all index files, then all .tex files again
 for tex in texfiles:
     print(tex, " Pass 1")
     subprocess.call([pdflatex, tex])
-
     print(tex, " Generating indices")
     for sxd in glob.glob('*.sxd'):
         subprocess.call([songidx, '-b', bible, sxd])
-
     print(tex, " Pass 2")
     subprocess.call([pdflatex, tex])
