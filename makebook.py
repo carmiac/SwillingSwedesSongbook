751a627505ee9fa69a8fc29acf16b67a28b7d248
Adam Milner <carmiac@gmail.com>
2015-09-10 21:05:07 -0700

    Fixed makebook.py for osx, and probably any *nix. Fixed some weird encoding issues in a few songs.

--- makebook.py
+++ makebook.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python3
+
 import glob
 import os
 import fileinput
 import subprocess
+import platform
+import shutil
 
-song_glob = "src\*.txt"
+# first, set up a bunch of paths for later
+if platform.system == 'Windows':
+    songidx = '..\src\songidx.exe'
+    bible = '..\src\songidx\bible.can'
+    pdflatex = 'C:\Program Files (x86)\MiKTeX 2.9\miktex\bin\pdflatex.exe'
+
+else:
+    songidx = '../src/songidx/songidx'
+    bible = '../src/songidx/bible.can'
+    pdflatex = 'pdflatex'
+    shutil.copy2('../src/songs/songs.sty', './songs.sty')
+
+song_glob = os.sep.join(['src','*.txt'])
 sbd_header = "header.txt"
 sbd_out = "songs.sbd"
 
@@ -15,36 +31,21 @@ with open(sbd_out, 'w') as fout:
     # add the header boilerplate
     for line in fileinput.input(sbd_header):
         fout.write(line)
-    # add the individual files 
+    # add the individual files
     for line in fileinput.input(filenames):
         fout.write(line)
 
-# and then run the generator batch file
-#subprocess.Popen("generate.bat -nopause")
-
-# replacing generate.bat
-
-# first, set up a bunch of paths for later
-scripts = '..\src'
-auxdir = 'temp'
-songidx = '..\src\songidx'
-sbdchk = '..\src\sbdchk'
-miktexpath = 'C:\Program Files (x86)\MiKTeX 2.9'
-miktexbin= os.sep.join([miktexpath,'miktex','bin'])
-
 # next, get a list of .tex files to use
 texfiles = sorted(glob.glob('*.tex'))
-print(texfiles)
+
 # then, regenerate all .tex files, all index files, then all .tex files again
 for tex in texfiles:
     print(tex, " Pass 1")
-    subprocess.call([os.sep.join([miktexbin, 'pdflatex.exe']), '='.join(['-aux-directory',auxdir]), tex])
-    
-    print(tex, " Generating indices")
-    for idx in glob.glob(os.sep.join([auxdir, '*.sxd'])):
-        subprocess.call([os.sep.join([songidx, 'songidx.exe']), '-b', os.sep.join([songidx, 'bible.can']), idx])
-    
-    print(tex, " Pass 2")
-    subprocess.call([os.sep.join([miktexbin, 'pdflatex.exe']), '='.join(['-aux-directory',auxdir]), tex])    
+    subprocess.call([pdflatex, tex])
 
-        
+    print(tex, " Generating indices")
+    for sxd in glob.glob('*.sxd'):
+        subprocess.call([songidx, '-b', bible, sxd])
+
+    print(tex, " Pass 2")
+    subprocess.call([pdflatex, tex])
